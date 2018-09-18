098bcaee19225bd94f26cb09e874e5fd0bc187e7
Ipung Purwono <32335074+banyucenter@users.noreply.github.com>
2018-09-18 11:20:36 +0700

    Update init.js

--- data/init.js
+++ data/init.js
@@ -9,7 +9,7 @@ var password = crypto.createHmac('sha256', secret)
 console.log("Password: " + password);
 
 //var mongoose = require('mongoose');
-let dev_db_url = 'mongodb://ipungdev:AllahMahaKaya9900@ds219051.mlab.com:19051/mo_purwokertojs';
+let dev_db_url = 'mongodb://ipungdev:yourpassword@ds219051.mlab.com:19051/nama_database';
 let mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
